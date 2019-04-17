2507d0047b84b2e8a3e939a1d627061176420df7
Reminem <shadow38101>
2019-04-16 20:05:31 -0400

    working with good output

--- genetic.py
+++ genetic.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd
+import pandas
 import operator
 import random
 import sys
@@ -7,60 +7,72 @@ import multiprocessing
 import time
 import objects
 
-
-
-def create_route(cityList):
-    route = random.sample(cityList, len(cityList))
+# Randomizes the cities and returns as a route
+def create_route(cities):
+    size = len(cities)
+    route = random.sample(cities, size)
+    # print(route, "\n")
     return route
 
-def initial_population(pop_size, city_list):
+# Creates a population of size size of routes
+def initial_population(size, cities):
     population = []
-    for i in range(0, pop_size):
-        population.append(create_route(city_list))
+    for i in range(size):
+        population.append(create_route(cities))
     return population
 
+# Ranks each route in the population and sorts it by their fitness
 def rank_routes(population):
-    return_vals = {}
+    fitness = {}
     for i in range(len(population)):
-        return_vals[i] = objects.Fitness(population[i]).route_fitness()
-    return sorted(return_vals.items(), key = operator.itemgetter(1), reverse = True) 
+        fitness[i] = objects.Fitness(population[i]).route_fitness()
+    sorted_list = sorted(fitness.items(), key = operator.itemgetter(1), reverse = True) 
+    # print(sorted_list,'\n')
+    return sorted_list
 
-def selection(pop_ranked, save_size):
-    return_vals = []
-    df = pd.DataFrame(np.array(pop_ranked), columns=["Index","Fitness"])
-    df['cum_sum'] = df.Fitness.cumsum()
-    df['cum_perc'] = 100*df.cum_sum/df.Fitness.sum()
-    for i in range(save_size):
-        return_vals.append(pop_ranked[i][0])
-    for i in range(len(pop_ranked) - save_size):
-        pick = random.random() * 100
-        for i in range(len(pop_ranked)):
-            if pick <= df.iat[i,3]:
-                return_vals.append(pop_ranked[i][0])
+# Selects a population of routes and saves selected indexes routes 
+def mate(ranked_population, save_size,  population):
+    selected = []
+    data_frame = pandas.DataFrame(np.array(ranked_population), columns = ["Index", "Fitness"]) # Two-dimensional size-mutable with labeled axes (rows and columns)
+    data_frame['cum_sum'] = data_frame.Fitness.cumsum()
+    data_frame['percentage'] = (data_frame.cum_sum * 100) / data_frame.Fitness.sum()
+    # print(data_frame)
+    for i in range(save_size): # Save the top 'save_size' contenders
+        selected.append(ranked_population[i][0])
+    for i in range((len(ranked_population) - save_size)):
+        random_pick = random.random() * 100
+        for i in range(len(ranked_population)):
+            if random_pick <= data_frame.iat[i, 3]:
+                selected.append(ranked_population[i][0])
                 break
-    return return_vals
-
-def mate(pop, select):
+    # print(select)
     pool = []
-    for i in range(len(select)):
-        index = select[i]
-        pool.append(pop[index])
+    for i in range(len(selected)):
+        index = selected[i]
+        # print(population[index])
+        pool.append(population[index])
+    # print(pool)
     return pool
 
+# Takes two parents, get random 'genes' from them and splice them together in the same order
 def breed(father, mother):
     child = []
-    child_father = []
-    child_mother = []
+    genes_father = []
+    genes_mother = []
     gene1 = int(random.random() * len(father))
     gene2 = int(random.random() * len(mother))
     start = min(gene1, gene2)
     end = max(gene1, gene2)
     for i in range(start, end):
-        child_father.append(father[i])
-    child_mother = [item for item in mother if item not in child_father]
-    child = child_father + child_mother
+        genes_father.append(father[i])
+    # print(genes_father)
+    genes_mother = [item for item in mother if item not in genes_father] # Keep genes not being kept in father's
+    # print(genes_mother)
+    child = genes_father + genes_mother
+    # print(child)
     return child
 
+# Runs breeding function over the entire population but saving 'save_selection' of top routs without changing
 def breed_pop(pool, save_size):
     children = []
     keep = len(pool) - save_size
@@ -68,62 +80,72 @@ def breed_pop(pool, save_size):
     for i in range(save_size):
         children.append(pool[i])
     for i in range(keep):
-        child = breed(selection[i], selection[len(pool)-i-1])
+        child = breed(selection[i], selection[len(pool) - i - 1])
         children.append(child)
     return children
 
+# Randomly 'mutates' a child by swapping random locations
 def mutate(child, mutation_rate):
     for i in range(len(child)):
         if (random.random() < mutation_rate):
             j = int(random.random() * len(child))
-            town1 = child[i]
-            town2 = child[j]
-            child[j] = town1
-            child[i] = town2
+            city1 = child[i]
+            city2 = child[j]
+            child[j] = city1
+            child[i] = city2
     return child
 
+# Runs mutate function over entire population
 def mutate_pop(population, mutation_rate):
     mutated = []
     for i in range(len(population)):
-        new_child = mutate(population[i], mutation_rate)
-        mutated.append(new_child)
+        child = mutate(population[i], mutation_rate)
+        mutated.append(child)
     return mutated
 
+# Takes a generation, ranks them, mates them and saves the next generation
 def next_generation(current_generation, save_size, mutation_rate):
     pop_ranked = rank_routes(current_generation)
-    selec = selection(pop_ranked, save_size)
-    pool = mate(current_generation, selec)
+    pool = mate(pop_ranked, save_size, current_generation)
     children = breed_pop(pool, save_size)
     next_generation = mutate_pop(children, mutation_rate)
     return next_generation
 
-def genetic_algorithm(population, pop_size, save_size, mutation_rate, generations, t_end):
-    pop = initial_population(pop_size, population)
-    print("Initial distance: " + str(1 / rank_routes(pop)[0][1]))
+# Runs program over certain time limit or till all generations have been made
+def find_tsp(population, pop_size, save_size, mutation_rate, generations, t_end):
+    current_gen = initial_population(pop_size, population)
     for i in range(generations):
         if time.time() >= t_end:
             print('Time limit exceeded. Ending Program.')
             break
-        pop = next_generation(pop, save_size, mutation_rate)
-    print('Final distance:', str(1 / rank_routes(pop)[0][1]))
-    best_route = pop[rank_routes(pop)[0][0]]
-    return best_route
+        current_gen = next_generation(current_gen, save_size, mutation_rate)
+    best_route = current_gen[rank_routes(current_gen)[0][0]]
+    distance = str(1 / rank_routes(current_gen)[0][1])
+    return best_route, distance
 
+# Create timer and run code
 def main():
     if len(sys.argv) < 4:
         print('Need correct args, run with: \ngenetic.py <input> <output> <time>')
         return
-    t_end = time.time() + int(sys.argv[3])
+    t_end = time.time() + int(sys.argv[3]) - 1
     cityList = []
     input_file = sys.argv[1]
+    output_file = sys.argv[2]
     f = open(input_file, "r") # Open file
     for line in f:
         line = line.strip('\n') # Strip off new line character
         split = line.split(" ", 3) # Split into three parts
-        cityList.append(objects.Town(x=float(split[1]), y=float(split[2])))
+        cityList.append(objects.City(x = int(float(split[1])), y = int(float(split[2])), node_id = int(split[0])))
     f.close()
-    
-    rout = genetic_algorithm(population = cityList, pop_size = 250, save_size = 20, mutation_rate = 0.01, generations = 1500, t_end = t_end)
+    print('Running for ' + sys.argv[3] + ' seconds.')
+    rout, distance = find_tsp(population = cityList, pop_size = 250, save_size = 20, mutation_rate = 0.01, generations = 150000, t_end = t_end)
+    f = open(output_file, 'w')
+    f.write(distance + '\n')
+    for city in rout:
+        f.write(str(city))
+        f.write(' ')
+    f.write(str(rout[0]))
     print(rout)
 
 if __name__ == "__main__":
--- objects.py
+++ objects.py
@@ -1,38 +1,47 @@
-
 import numpy as np
 
-
-class Town:
-    def __init__(self, x, y):
+# City object
+class City:
+    # Initializes the object
+    def __init__(self, x, y, node_id):
         self.x = x
         self.y = y
+        self.node_id = node_id
 
-    def distance(self, town):
-        dist_x = abs(self.x - town.x)
-        dsit_y = abs(self.y - town.y)
+    # Finds distance between two cities
+    def distance(self, city):
+        dist_x = abs(self.x - city.x)
+        dsit_y = abs(self.y - city.y)
         distance = np.sqrt((dist_x ** 2) + (dsit_y ** 2))
         return distance
+    # Sends over the node ID
+    def __repr__(self):
+        return str(self.node_id)
 
+# Crates a fitness object to see how good the route is
 class Fitness:
+    # Initializes and sets everything to 0
     def __init__(self, route):
         self.route = route
         self.distance = 0
         self.fitness = 0
 
+    # Gets distance for the route
     def route_distance(self):
         if self.distance == 0:
-            path_distance = 0
+            dist = 0
             for i in range(len(self.route)):
-                from_city = self.route[i]
-                to_city = None
-                if i + 1 < len(self.route):
-                    to_city = self.route[i + 1]
+                c_from = self.route[i]
+                c_to = None
+                if (i + 1) < len(self.route):
+                    c_to = self.route[i + 1]
                 else:
-                    to_city = self.route[0]
-                path_distance += from_city.distance(to_city)
-            self.distance = path_distance
+                    c_to = self.route[0]
+                dist += c_from.distance(c_to)
+            self.distance = dist
         return self.distance
 
+    # Calculates the fitness of the route, higher is better
     def route_fitness(self):
         if self.fitness == 0:
             self.fitness = 1 / float(self.route_distance())
